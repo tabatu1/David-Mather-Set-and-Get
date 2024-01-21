e50f070bfeeefe86d8aa43488cb0ff2ea5845ebe
Dave Mather <34279071+tabatu1@users.noreply.github.com>
2024-01-21 19:19:52 +0000

    Delete GetterSetter-main directory

--- GetterSetter-main/README.md
+++
@@ -1,2 +0,0 @@
-# GetterSetter
-Deploys gets and sets and presents results from a Solidity contract
--- GetterSetter-main/scripts/dave_mather_start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd scripts
-./dave_mather_deploy_set_get.sh -v "$VAL" -a $APIURLVAL -p $PRKEY
-
-
-
